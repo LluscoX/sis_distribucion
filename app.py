b2d6802930498f9caead7f43bb5903a4d62ac88f
Brayan <lluscovillarpando1@gmail.com>
2024-11-03 21:49:05 -0400

    antes de modificar todo pal MVC

--- app.py
+++ app.py
@@ -3,6 +3,29 @@
 from flask import Flask, render_template_string, render_template, url_for, request, redirect, session, flash, Response
 import folium
 
+from folium import IFrame
+import json
+# import cv2
+
+usuarios =  {
+    "usuario1": {
+        "correo": "usuario1@gmail.com", 
+        "clave": "123", 
+        "rol" : "vendedor"
+    },
+    "usuario2": {
+        "correo": "usuario2@gmail.com", 
+        "clave": "456",
+        "rol": "vendedor"
+    },
+    "usuario3": {
+        "correo": "usuario3@gmail.com",
+        "clave": "789",
+        "rol": "gerente"
+    }
+}
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +35,27 @@ def home():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/validar_login', methods=['POST'])
+def validar_login():
+    usuario_formulario = request.form['username']
+    clave_formulario = request.form['password']
+    
+    if(usuario_formulario in usuarios):
+        user_data = usuarios[usuario_formulario]
+        user_login = user_data
+        user_clave = user_data['clave']
+        user_rol = user_data['rol']
+        if(user_clave == clave_formulario):
+            session['usuarios_logueado'] = usuario_formulario
+            session['rol'] = user_rol
+            return render_template('index.html')
+    else:
+        return redirect(url_for('login'))
+
+
+
 
 @app.route('/mapa')
 def mapa():
@@ -37,25 +81,67 @@ def ver_mapa():
     direccion = 'calle La Tablada # 4533'
     fecha = '10 Agosto 2024'
 
-    htmlcode = f"""
-    <table border="1" class="table table-success table-striped">
-        <tr><td colspan="2"><img src='{foto_url}' width='250' height='200'></td></tr>
-        <tr><td>Tienda:</td><td>{nombre_tienda}</td></tr>
-        <tr><td>Contacto:</td><td>{contacto}</td></tr>
-        <tr><td>Dirección:</td><td>{direccion}</td></tr>
-        <tr><td>Fecha:</td><td>{fecha}</td></tr>
-        <tr><td colspan="2"><center><a class="btn btn-primary" href="{url_for('pedido')}" style="color: white;">Hacer Pedido</a></center></td></tr>
-    </table>"""
+    tiendas = [
+        {   'nombre_tienda': 'Doña filomena',
+            'contacto': 'Filomena Delgado',
+            'direccion': 'calle La Tablada # 4533',
+            'fecha': '10 Agosto 2024',
+            'coordenadas': [-17.3935, -66.1570],
+            'foto': 'tienda_barrio.jpg'
+        },
 
-    folium.Marker(
-        location=[-17.3935, -66.1570],
-        popup=htmlcode,
-        tooltip='Haz click para más información'
-    ).add_to(m)
+        {   'nombre_tienda': 'Tienda Maria',
+            'contacto': 'Juanita perez',
+            'direccion': 'Av. Trinidad y Tomas Oconor # 1020',
+            'fecha': '15 Agosto 2024',
+            'coordenadas': [-17.375906, -66.157694],
+            'foto': './images/tienda_Maria.png'
+        },
 
-    
-    # path = '/home/patitofeo/mysite/mapa_cbb2.html'
-    # m.save(path)
+        {   'nombre_tienda': 'Tiena Pepe',
+            'contacto': 'Oscar flores',
+            'direccion': 'Av. Melchor urquidi y zenon salinas # 1320',
+            'fecha': '25 Agosto 2024',
+            'coordenadas': [-17.374338, -66.149587],
+            'foto': './images/tienda_Pepe.png'
+        },
+
+        {   'nombre_tienda': 'Tiena Don aguilar',
+            'contacto': 'Pedro aguilar',
+            'direccion': 'Av. Santa cruz y Buenos Aires # 1253',
+            'fecha': '22 Agosto 2024',
+            'coordenadas': [-17.374155, -66.156953],
+            'foto': './images/tienda_Pep.png'
+        },
+        {   'nombre_tienda': 'Tiena Copacabana',
+            'contacto': 'Luis Miguel',
+            'direccion': 'Av. Portales y A.M. Torrico # 1254',
+            'fecha': '05 Octubre 2024',
+            'coordenadas': [-17.375054, -66.155478],
+            'foto': './images/tienda_Copacabana.png'
+        }
+    ]
+
+    for tienda in tiendas:
+        foto_url = url_for('static', filename=tienda['foto'])  # Obtener la URL de la foto
+
+        # Crear el código HTML para el popup de cada tienda
+        htmlcode = f"""
+        <table border="1" class="table table-success table-striped">
+            <tr><td colspan="2"><img src='{foto_url}' width='250' height='200'></td></tr>
+            <tr><td>Tienda:</td><td>{tienda['nombre_tienda']}</td></tr>
+            <tr><td>Contacto:</td><td>{tienda['contacto']}</td></tr>
+            <tr><td>Dirección:</td><td>{tienda['direccion']}</td></tr>
+            <tr><td>Fecha:</td><td>{tienda['fecha']}</td></tr>
+            <tr><td colspan="2"><center><a class="btn btn-primary" href="{url_for('pedido')}" style="color: white;">Hacer Pedido</a></center></td></tr>
+        </table>"""
+
+        folium.Marker(
+            location=tienda['coordenadas'],
+            popup=htmlcode,
+            tooltip=f"{tienda['nombre_tienda']}: Haz click para más información"
+        ).add_to(m)
+ 
 
     # Usar el método correcto para generar el HTML del mapa
     mapa_html = m._repr_html_()
